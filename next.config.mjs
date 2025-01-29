fdfd7275825f337ce916d3cde3670282025dca04
Nemi35 <89382560+Nemi35@users.noreply.github.com>
2025-01-29 23:54:01 +0530

    fix the api of event

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      appDir: true,  // Enable the app directory feature for Next.js 13+
+    },
+  };
+  
+  export default nextConfig;
+  
